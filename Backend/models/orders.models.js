fb002c2f3a9ae426a2ef1f20a97b58a7236fed13
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-04-28 17:53:05 +0530

    addordermodels

---
+++ Backend/models/orders.models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    cart: [
+      {
+        itemId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "MenuItem",
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
